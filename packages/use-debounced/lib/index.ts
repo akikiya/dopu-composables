8a3b34e3ea1cca76d13a74148a258c079300487c
akikiya <2775686007@qq.com>
2024-07-07 20:03:26 +0800

    feat(use-debounced): add flush and cancel methods to debounced function
    Extend the functionality of the useDebouncedFn composable to include `flush` and `cancel`
    methods. The `flush` method forces the debounced function to execute immediately, while
    the `cancel` method prevents any pending executions. These enhancements allow for more
    fine-grained control over the debounced function's behavior.
    
    BREAKING CHANGE: The return type of useDebouncedFn has changed to include the flush and
    cancel methods, which may affect consumers depending on the previous return type.

--- packages/use-debounced/lib/index.ts
+++ packages/use-debounced/lib/index.ts
@@ -1,27 +1,62 @@
 import { customRef, Ref } from "vue"
 
+interface DebouncedFn<T, A extends any[]> {
+  (this: T, ...args: A): void
+  /**
+   * Flushes the debounced function regardless of whether it has been cancelled or not.
+   */
+  flush: (this: T, ...args: A) => void
+  /**
+   * Cancels the debounced function.
+   */
+  cancel(): void
+}
+
 /**
  * Provides a debounced function.
  * @param fn The function to debounce.
- * @param delay The zero or greater delay in milliseconds. The default is 200.
- * @returns The debounced function.
+ * @param delay The zero or greater delay in milliseconds. The default is `200`.
+ * @returns The debounced function with `flush` and `cancel` methods.
+ *
+ * @example
+ * ```js
+ * const log = useDebouncedFn(console.log, 1000)
+ * log(1)
+ * log(2)
+ * log(3) // logs 3 after 1000ms
+ * log.cancel()
+ * log(1) // nothing is logged
+ * log.flush(1, 2, 3) // logs 1, 2, 3 immediately
+ * ```
  */
 function useDebouncedFn<T, A extends any[]>(
   fn: (this: T, ...args: A) => any,
   delay: number = 200
-): (this: T, ...args: A) => void {
+): DebouncedFn<T, A> {
   if (typeof fn !== "function" || typeof delay !== "number") {
     throw new TypeError(
       "useDebouncedFn: fn and delay must be a function and a number"
     )
   }
   let timeout: number
-  return function debounced(this: T, ...args: A) {
+  let cancelled = false
+  function debounced(this: T, ...args: A) {
+    if (cancelled) {
+      return
+    }
     clearTimeout(timeout)
     timeout = setTimeout(() => {
       fn.apply(this, args)
     }, delay)
   }
+  debounced.flush = function (this: T, ...args: A) {
+    clearTimeout(timeout)
+    fn.apply(this, args)
+  }
+  debounced.cancel = () => {
+    cancelled = true
+  }
+  return debounced
 }
 
 /**
