8c508eeb2448eb0dad53d20fe8bf3087e953cc0e
akikiya <2775686007@qq.com>
2024-07-09 11:17:08 +0800

    feat(composables-core): add collect modules step to build:docs script
    The build:docs script within the composable-core package has been updated to
    include a new step for collecting modules. This change ensures that the
    documentation building process is more efficient and streamlined by
    pre-calling the collect:modules script before generating the typedoc.

--- packages/composables-core/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '../../use-debounced/lib/index.ts'
-export * from '../../use-media-query/lib/index.ts'
-export * from '../../use-mounted/lib/index.ts'
-export * from '../../use-throttled/lib/index.ts'
